319be39f0bf36675fb8728945f4c02a4ef8ce40d
Benj Kamm <bkamm@glidian.com>
2021-01-27 15:34:16 -0600

    add postJson helper

--- src/api.js
+++ src/api.js
@@ -3,6 +3,17 @@ async function fetchJson(...args) {
   return resp.json();
 }
 
+// eslint-disable-next-line no-unused-vars
+function postJson(url, body) {
+  return fetchJson(url, {
+    body,
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
+
 export function listGroups() {
   return fetchJson("/groups");
 }
